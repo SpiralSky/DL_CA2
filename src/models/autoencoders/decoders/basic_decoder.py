8818f258569400bf862ac44868cb4e8132a899fb
spiralsky <shianastrophysics@gmail.com>
2026-08-07 20:01:42 +0800

    Updated type annotations

--- src/models/autoencoders/decoders/basic_decoder.py
+++ src/models/autoencoders/decoders/basic_decoder.py
@@ -1,14 +1,23 @@
+"""
+Module containing basic decoder
+"""
+
 import torch.nn as nn
+
 
 class BasicDecoder(nn.Module):
     def __init__(self, latent_dim=256):
+        """
+        Basic Decoder class for standard VAE implementations.
+        :param latent_dim: Latent dimension size.
+        """
         super().__init__()
 
         # W/H Dimension of the first image (after Unflatten).
         self.init_spatial = 4
 
         # Image dimensions (W/H):
-        # 4 -> 8 -> 16 -> 32
+        # 4 -> 8 -> 16 -> 32A
         self.decoder = nn.Sequential(
             nn.Linear(latent_dim, 4096),
             nn.Unflatten(dim=1, unflattened_size=(256, self.init_spatial, self.init_spatial)),
@@ -22,6 +31,14 @@ class BasicDecoder(nn.Module):
 
     @staticmethod
     def make_conv_block(in_channels: int, out_channels: int, upsample=True) -> nn.Sequential:
+        """
+        Helper function to create a convolutional block.
+        By default, an upsample layer doubles spatial dimension magnitudes (W/H).
+        :param in_channels: Input channel count.
+        :param out_channels: Output channel count. Also represents the number of convolutional filters that will be used.
+        :param upsample: Whether to upsample image. If false, will retain image spatial dimensions (W/H).
+        :return: Sequential layer (convolution block).
+        """
         conv_block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -38,4 +55,9 @@ class BasicDecoder(nn.Module):
 
 
     def forward(self, input_features):
+        """
+        Standard forward pass.
+        :param input_features: Input feature maps.
+        :return:
+        """
         return self.decoder(input_features)
--- src/models/autoencoders/encoders/basic_encoder.py
+++ src/models/autoencoders/encoders/basic_encoder.py
@@ -4,9 +4,8 @@ import torch.nn as nn
 class BasicEncoder(nn.Module):
     def __init__(self, latent_dim: int = 256):
         """
-        Basic Encoder
-        Input: Image with 3 channels
-        Output: Latent Vector of mean and log variance of length lat
+        Basic Encoder class.
+        :param latent_dim: Length of latent dimension vector.
         """
         super().__init__()
 
@@ -26,6 +25,13 @@ class BasicEncoder(nn.Module):
 
     @staticmethod
     def down_block(in_channels: int, out_channels: int) -> nn.Sequential:
+        """
+        Standard downsampling block. Kernel_size=3, stride=2 and padding=1 causes spatial dimensions (W/H) to halve.
+        BatchNormalization is used on raw logits to prevent gradient shrinking.
+        :param in_channels: Input channels.
+        :param out_channels: Output channels. Also corresponds to number of convolutional filters used.
+        :return:
+        """
         return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -33,6 +39,11 @@ class BasicEncoder(nn.Module):
         )
 
     def forward(self, inputs):
+        """
+        Standard forward pass. Uses inherited mean and log_variance linear layers to output mean and log variance.
+        :param inputs: Feature maps.
+        :return: Tuple of mean and log variance.
+        """
         feature_maps = self.features(inputs)
         mean = self.mean(feature_maps)
         log_variance = self.log_variance(feature_maps)
